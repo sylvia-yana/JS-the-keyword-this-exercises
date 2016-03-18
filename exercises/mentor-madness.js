7cea1a7f5eb6156553951b410e28418861aa1a0e
Zachary Lester <zachary.greg.lester@gmail.com>
2016-03-18 09:43:52 -0700

    Further clarify prompt, add bonus

--- exercises/mentor-madness.js
+++ exercises/mentor-madness.js
@@ -12,13 +12,14 @@ var foo = {
 
 var findMentor = foo.bar.getMentor;
 
-// What would the following log?
+// What would the following log (in the browser)?
 // What binding pattern(s) cause this?
 foo.bar.getMentor();
 findMentor();
 
-// How would you modify the following invocations of findMentor 
-// to log each of the mentor's names?
+// How would you modify the following invocations of findMentor to log each of the mentor's names?
 findMentor(); // Should log 'Zach'
 findMentor(); // Should log 'Sunny'
 findMentor(); // Should log 'Beth'
+
+// Bonus: Does findMentor behave differently in the browser vs. when run by Node? If so, why?
